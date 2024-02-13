cc333633380d020d0638f4aff00be4d03970e3d4
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2024-02-13 12:30:30 -0300

    feat: add modules to main

--- annotations-backend/src/app.module.ts
+++ annotations-backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AnnotationsModule } from './modules/annotations/annotations.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AnnotationsModule, CategoriesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
